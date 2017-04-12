d07f92731facd465d29209d0b830b0ad383495c3
Gene Sy <gnsyftw@gmail.com>
2017-04-12 15:56:23 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# react-native-hr
+
+<Hr> Tag for React Native
+
+
+## Usage
+
+
+### Basic Usage
+```
+  <Hr />
+```
+
+### With components inside
+```
+  <Hr><Text>Centered Text</Text></Hr>
+  <Hr><Image /></Hr>
+```
+
+
+### With Alignments
+#### Left Algined
+```
+  <Hr align="left"><Text>Left Aligned Text</Text></Hr>
+  <Hr align="left"><Image /></Hr>
+```
+
+#### Right Aligned
+```
+  <Hr align="right"><Text>Right Aligned Text</Text></Hr>
+  <Hr align="right"><Image /></Hr>
+```
+
+### With styles
+
+```
+  <Hr style={{background:'yellow', color:'red'}}><Text>Test</Text></Hr>
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+class Hr extends Component {
+  getStyle() {
+    return StyleSheet.create({
+      line: {
+        backgroundColor: this.props.style.color,
+        height: this.props.style.height || 1,
+        flex: 1
+      },
+      wrapper: {
+        flexDirection: 'row',
+        alignItems:'center',
+        backgroundColor: this.props.style.backgroundColor || 'transparent'
+      }
+    })
+  }
+  render() {
+    if(!this.props.children) {
+      return (
+        <View style={this.getStyle().wrapper}>
+          <HrLine style={this.getStyle().line}/>
+        </View>
+      )
+    }
+    return (
+      <View style={this.getStyle().wrapper}>
+        {
+          (this.props.align === 'left') ? <View/> : <HrLine style={this.getStyle().line}/>
+        }
+        { this.props.children }
+        {
+          (this.props.align === 'right') ? <View/> : <HrLine style={this.getStyle().line}/>
+        }
+      </View>
+    );
+  }
+}
+
+Hr.defaultProps = {
+  style: {
+    color: 'black',
+    height: 1,
+    backgroundColor: 'transparent'
+  }
+}
+class HrLine extends Component {
+  render() {
+    return (
+      <View style={this.props.style}/>
+    );
+  }
+}
+
+export default  Hr;
